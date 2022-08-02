903b804b1b6eb285ae2688525325e2f85a40500f
Ajay Nerurkar <ajay@atimotors.com>
2022-08-02 11:09:59 +0530

    Rename test directory to tests
    This avoids a conflict with python's standard test package

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,9 @@
 import time
 from core.db import engine
 from models import fleet_models, trip_models
-from models.db_session import DBSession, add_sherpa
+from models.db_session import DBSession
 from models.fleet_models import Sherpa
+from utils.fleet_utils import add_fleet, add_sherpa, add_sherpa_to_fleet, add_station
 
 
 def test_sherpa_model():
@@ -64,6 +65,24 @@ def test_trip_model():
     sess.end_leg("S1")
     sess.end_trip("S1")
     sess.close()
+
+
+def curl_test():
+    fleet_models.Base.metadata.drop_all(bind=engine)
+    trip_models.Base.metadata.drop_all(bind=engine)
+    fleet_models.Base.metadata.create_all(bind=engine)
+    trip_models.Base.metadata.create_all(bind=engine)
+    add_fleet("test")
+    api_key = add_sherpa("S2", "efgh")
+    print(api_key)
+    add_sherpa_to_fleet("S2", "test")
+    add_station("a", [1.0, 0.0, 3.14])
+    add_station("b", [2.0, 0.0, 3.14])
+    add_station("c", [3.0, 0.0, 3.14])
+    dbs = DBSession()
+    trip = dbs.create_trip(["a", "b", "c"])
+    dbs.create_pending_trip(trip.id)
+    dbs.close()
 
 
 if __name__ == "__main__":
