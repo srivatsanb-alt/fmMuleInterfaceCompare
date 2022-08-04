5b0c3b62ef22765c4ca18c2a05411ed95908fee9
Ajay Nerurkar <ajay@atimotors.com>
2022-08-04 11:23:34 +0530

    Add models for move, reached and peripherals messages

--- endpoints/request_models.py
+++ endpoints/request_models.py
@@ -1,7 +1,15 @@
+from enum import Enum
 from typing import List, Union
 from pydantic import BaseModel
 
 from core.constants import MessageType
+
+# Messages from sherpas to FM
+
+
+class SherpaMsg(BaseModel):
+    source: Union[str, None] = None
+    type: str
 
 
 class InitExtraInfo(BaseModel):
@@ -10,8 +18,52 @@ class InitExtraInfo(BaseModel):
     chassis_number: str
 
 
-class InitMsg(BaseModel):
+class InitMsg(SherpaMsg):
     current_pose: List[float]
-    source: Union[str, None] = None
     extra_info: Union[InitExtraInfo, None] = None
     type = MessageType.INIT
+
+
+class ReachedMsg(SherpaMsg):
+    trip_id: int
+    trip_leg_id: int
+    destination_pose: List[float]
+    destination_name: str
+    type = MessageType.REACHED
+
+
+# Messages from FM to sherpas
+class FMCommand(BaseModel):
+    endpoint: str
+
+
+class DirectionEnum(str, Enum):
+    send = "send"
+    receive = "receive"
+
+
+class MoveMsg(FMCommand):
+    endpoint: str = "move_to"
+    trip_id: int
+    trip_leg_id: int
+    destination_pose: List[float]
+    destination_name: str
+
+
+class HitchMsg(BaseModel):
+    hitch: bool
+
+
+class ConveyorMsg(BaseModel):
+    direction: DirectionEnum
+    num_units: int
+
+
+class DispatchButtonMsg(BaseModel):
+    value: bool
+
+
+class PeripheralsMsg(FMCommand):
+    endpoint: str = "peripherals"
+    hitch_msg: HitchMsg = None
+    conv_msg: ConveyorMsg = None
