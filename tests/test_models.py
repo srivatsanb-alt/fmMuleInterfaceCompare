f1f0af86927b06eb19abf76a9f2d631b5a0b6340
Ajay Nerurkar <ajay@atimotors.com>
2022-08-10 17:59:40 +0530

    Update tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,8 +2,7 @@ import time
 from core.db import engine
 from models import fleet_models, trip_models
 from models.db_session import DBSession
-from models.fleet_models import Sherpa
-from utils.fleet_utils import add_fleet, add_sherpa, add_sherpa_to_fleet, add_station
+from utils.fleet_utils import add_update_fleet, add_sherpa, add_sherpa_to_fleet, add_station
 
 
 def test_sherpa_model():
@@ -12,11 +11,8 @@ def test_sherpa_model():
     fleet_models.Base.metadata.create_all(bind=engine)
     trip_models.Base.metadata.create_all(bind=engine)
 
-    add_sherpa("S1")
+    add_sherpa("S1", "abcd")
     sess = DBSession()
-    sherpa: Sherpa = sess.get_sherpa("S1")
-    sherpa.initialized = True
-    print(sherpa)
     sess.close()
 
 
@@ -26,7 +22,7 @@ def test_trip_model():
     fleet_models.Base.metadata.create_all(bind=engine)
     trip_models.Base.metadata.create_all(bind=engine)
 
-    add_sherpa("S1")
+    add_sherpa("S1", "abcd")
 
     sess = DBSession()
     sess.new_trip(route=["a", "b", "c"])
@@ -72,10 +68,10 @@ def curl_test():
     trip_models.Base.metadata.drop_all(bind=engine)
     fleet_models.Base.metadata.create_all(bind=engine)
     trip_models.Base.metadata.create_all(bind=engine)
-    add_fleet("test")
-    api_key = add_sherpa("S2", "efgh")
+    add_update_fleet(name="test")
+    api_key = add_sherpa("S1", "abcd")
     print(api_key)
-    add_sherpa_to_fleet("S2", "test")
+    add_sherpa_to_fleet("S1", "test")
     add_station("a", [1.0, 0.0, 3.14])
     add_station("b", [2.0, 0.0, 3.14])
     add_station("c", [3.0, 0.0, 3.14])
@@ -85,11 +81,40 @@ def curl_test():
     dbs.close()
 
 
+def hall1_test():
+    dbs = DBSession()
+    trip_models.Trip.__table__.drop(bind=engine)
+    trip_models.OngoingTrip.__table__.drop(bind=engine)
+    trip_models.PendingTrip.__table__.drop(bind=engine)
+    trip = dbs.create_trip(["Start", "S1", "Start"])
+    dbs.create_pending_trip(trip.id)
+    dbs.close()
+
+
 def init_test():
     dbs = DBSession()
     trip = dbs.create_trip(["a", "b", "c"])
     dbs.create_pending_trip(trip.id)
     dbs.close()
+
+
+def ws_conn_test():
+    from fastapi.testclient import TestClient
+    from app.main import app
+
+    client = TestClient(app)
+    headers = {"x-api-key": "MbviPsacmoa-hRB_iXp_HSpTsSTSE-YIbl3bHjCb4uM_abcd"}
+    with client.websocket_connect("/api/v1/sherpa/", headers=headers) as websocket:
+        data = {
+            "type": "sherpa_status",
+            "timestamp": 1654166684,
+            "sherpa_name": "S2",
+            "current_pose": [1.5, 2.5, 6.28],
+            "battery_status": 90,
+            "mode": "fleet",
+            "error": False,
+        }
+        websocket.send_json(data)
 
 
 if __name__ == "__main__":
