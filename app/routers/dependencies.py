6a30386c3c81c0099e7ecafeef70a113133c1329
Ajay Nerurkar <ajay@atimotors.com>
2022-08-11 13:19:38 +0530

    Close db session after getting sherpa

--- app/routers/dependencies.py
+++ app/routers/dependencies.py
@@ -7,4 +7,7 @@ def get_sherpa(x_api_key: str = Header(None)):
     if x_api_key is None:
         return None
     hashed_api_key = hashlib.sha256(x_api_key.encode("utf-8")).hexdigest()
-    return session.get_sherpa_by_api_key(hashed_api_key)
+    sherpa = session.get_sherpa_by_api_key(hashed_api_key)
+    session.close()
+
+    return sherpa
