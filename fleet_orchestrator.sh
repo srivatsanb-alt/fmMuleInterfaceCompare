fb8265ddfe33d66e8cc869835833ca3bd17c5f35
Sai Karthik <pixarsnewhero@gmail.com>
2022-08-24 17:08:40 +0530

    Updating fleet_orchestrator script

--- fleet_orchestrator.sh
+++ fleet_orchestrator.sh
@@ -1,4 +1,34 @@
+#! /bin/bash
+
 source env.sh
-poetry run uvicorn --reload app.main:app --host 0.0.0.0 > /tmp/temp.out 2>&1 &
-redis-cli flushall && rm -rf logs 
-poetry run python /app/main.py > /tmp/temp.out 2>&1  # (in another window)
+
+LOGS=$FM_LOG_DIR
+TS=$(date +'%H%M%S')
+
+shutdown() {
+    echo "shutting down fleet manager"
+    poetry run python scripts/shutdown.py
+    echo "shutting down uvicorn"
+    ps -f | grep uvicorn | awk '{print $2}' | xargs kill -9 >& /dev/null
+}
+
+start() {
+    echo "starting fleet manager"
+    poetry run python main.py > $LOGS/fm.out 2>&1 &
+    echo "starting uvicorn"
+    poetry run uvicorn app.main:app --host 0.0.0.0 > $LOGS/uvicorn.out 2>&1 &
+}
+
+save_fleet_log() {
+    echo "saving old fleet manager logs"
+    mkdir -p $LOGS/backup
+    for f in $(ls $LOGS); do
+      [[ "$f" == "backup" ]] && continue
+      mv $LOGS/$f $LOGS/backup/$f.$TS 2>/dev/null
+    done
+}
+
+shutdown
+save_fleet_log
+redis-cli flushall
+start
