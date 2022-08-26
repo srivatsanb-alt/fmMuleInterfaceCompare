592b5abc31fec0fc4b397672c7de7d36a5d0cac8
Ajay Nerurkar <ajay@atimotors.com>
2022-08-26 14:19:32 +0530

    Add visa type constants
    This reverts commit e5a5d071e64bb7bc778501107d3ff0a053cfadff.

--- core/constants.py
+++ core/constants.py
@@ -6,3 +6,16 @@ class MessageType:
     TRIP_STATUS = "trip_status"
     BOOKING = "book"
     PASS_TO_SHERPA = "pass_to_sherpa"
+    RESOURCE_ACCESS = "resource_access"
+
+
+class VisaType:
+    PARKING = "parking"
+    EXCLUSIVE_PARKING = "exclusive_parking"
+    UNPARKING = "unparking"
+    TRANSIT = "transit"
+
+
+class VisaAccessType:
+    REQUEST = "request"
+    RELEASE = "release"
