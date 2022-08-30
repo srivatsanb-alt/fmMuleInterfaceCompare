94873eb772eb5c0d2617a85d8542ded432b6620a
Ajay Nerurkar <ajay@atimotors.com>
2022-08-30 16:54:34 +0530

    Rename 'actual' to 'test' and add it to logs

--- utils/visa_utils.py
+++ utils/visa_utils.py
@@ -4,10 +4,10 @@ from models.db_session import session
 from core.logs import get_logger
 from models.fleet_models import Sherpa
 from models.request_models import VisaType
-from models.visa_models import ExclusionZone, LinkedGates
+from models.visa_models import ExclusionZone
 
 
-def lock_exclusion_zone(zone_name, zone_type, sherpa_name, exclusive=True, actual=True):
+def lock_exclusion_zone(zone_name, zone_type, sherpa_name, exclusive=True, test=False):
     zone_id = f"{zone_name}_{zone_type}"
     sherpa: Sherpa = session.get_sherpa(sherpa_name)
     ezone = session.get_exclusion_zone(zone_name, zone_type)
@@ -17,12 +17,16 @@ def lock_exclusion_zone(zone_name, zone_type, sherpa_name, exclusive=True, actua
         )
         return False
 
+    get_logger(sherpa.name).info(
+        f"{zone_id} access held by {[s.name for s in ezone.sherpas]}"
+    )
+
     if len(ezone.sherpas) == 0:
         # no other sherpa in ezone
         get_logger(sherpa.name).info(
-            f"{sherpa.name} granted lock for {zone_id},exclusive={exclusive} "
+            f"{sherpa.name} granted lock for {zone_id},exclusive={exclusive},test={test} "
         )
-        if actual:
+        if not test:
             ezone.sherpas = [sherpa]
             ezone.exclusivity = exclusive
         return True
@@ -30,28 +34,30 @@ def lock_exclusion_zone(zone_name, zone_type, sherpa_name, exclusive=True, actua
     if len(ezone.sherpas) == 1 and ezone.sherpas[0] == sherpa:
         # sherpa already has access
         get_logger(sherpa.name).info(
-            f"{sherpa.name} granted lock for {zone_id},exclusive={exclusive} "
+            f"{sherpa.name} granted lock for {zone_id},exclusive={exclusive},test={test}"
         )
-        if actual:
+        if not test:
             ezone.exclusivity = exclusive
         return True
 
     if ezone.exclusivity:
         # ezone already locked exclusively by another sherpa.
         get_logger(sherpa.name).info(
-            f"{sherpa.name} denied lock for {zone_id}, exclusive access held by {ezone.sherpas[0]}"
+            f"{sherpa.name} denied lock for {zone_id}, exclusive access held by {ezone.sherpas[0]},test={test}"
         )
         return False
     elif not exclusive:
         # can't lock exclusively since there are other sherpas in ezone.
         get_logger(sherpa.name).info(
-            f"{sherpa.name} granted lock for {zone_id},exclusive={exclusive}"
+            f"{sherpa.name} granted lock for {zone_id},exclusive={exclusive},test={test}"
         )
-        if actual and sherpa not in ezone.sherpas:
+        if not test and sherpa not in ezone.sherpas:
             ezone.sherpas.append(sherpa)
         return True
     else:
-        get_logger(sherpa.name).info(f"{sherpa.name} denied exclusive lock for {zone_id}")
+        get_logger(sherpa.name).info(
+            f"{sherpa.name} denied exclusive lock for {zone_id},test={test}"
+        )
         return False
 
 
@@ -70,7 +76,7 @@ def lock_linked_zones(zone_name, zone_type, sherpa_name):
 
     for lz in linked_gates:
         lz_name, lz_type = split_zone_id(lz.zone_id)
-        can_lock = lock_exclusion_zone(lz_name, lz_type, sherpa_name, actual=False)
+        can_lock = lock_exclusion_zone(lz_name, lz_type, sherpa_name, test=True)
         can_lock_linked_gates.append(can_lock)
 
     if all(can_lock_linked_gates):
