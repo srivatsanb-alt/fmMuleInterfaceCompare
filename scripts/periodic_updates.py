60b2a4d1451ce4147c0a2be2895037da9c66d3eb
Ajay Nerurkar <ajay@atimotors.com>
2022-09-01 13:26:37 +0530

    Check type of column rather than its value to handle None values

--- scripts/periodic_updates.py
+++ scripts/periodic_updates.py
@@ -2,28 +2,22 @@ from utils.comms import send_status_update
 import logging
 from core.db import session_maker
 from models.db_session import DBSession
-from models.fleet_models import (
-    SherpaStatus,
-    Sherpa,
-    Fleet,
-    Station,
-    StationStatus
-)
+from models.fleet_models import SherpaStatus, Sherpa, Fleet, Station, StationStatus
 import time
 import inspect
 
 
 def get_table_as_dict(model, model_obj):
-    all_valid_types = ['str', 'dict', 'list', 'int', 'float', 'bool']
-    cols = model.__table__.columns.keys()
+    all_valid_types = ["str", "dict", "list", "int", "float", "bool"]
+    cols = [(c.name, c.type.python_type.__name__) for c in model.__table__.columns]
     result = {}
     model_dict = model_obj.__dict__
-    for col in cols:
+    for col, col_type in cols:
         if col in ["created_at", "updated_at"]:
             pass
         elif inspect.isclass(model_dict[col]):
             pass
-        elif type(model_dict[col]).__name__ not in all_valid_types:
+        elif col_type not in all_valid_types:
             pass
         else:
             if isinstance(model_dict[col], list):
@@ -51,21 +45,32 @@ def get_fleet_status_msg(session, fleet):
     if all_sherpa_status:
         for sherpa_status in all_sherpa_status:
             if sherpa_status.sherpa.fleet.name == fleet.name:
-                sherpa_status_update.update({
-                                            sherpa_status.sherpa_name:
-                                            get_table_as_dict(SherpaStatus, sherpa_status)})
+                sherpa_status_update.update(
+                    {
+                        sherpa_status.sherpa_name: get_table_as_dict(
+                            SherpaStatus, sherpa_status
+                        )
+                    }
+                )
 
                 sherpa_status_update[sherpa_status.sherpa_name].update(
-                                            get_table_as_dict(Sherpa, sherpa_status.sherpa))
+                    get_table_as_dict(Sherpa, sherpa_status.sherpa)
+                )
 
     if all_station_status:
         for station_status in all_station_status:
             if station_status.station.fleet.name == fleet.name:
-                station_status_update.update({station_status.station_name:
-                                              get_table_as_dict(StationStatus, station_status)})
+                station_status_update.update(
+                    {
+                        station_status.station_name: get_table_as_dict(
+                            StationStatus, station_status
+                        )
+                    }
+                )
 
                 station_status_update[station_status.station_name].update(
-                                        get_table_as_dict(Station, station_status.station))
+                    get_table_as_dict(Station, station_status.station)
+                )
 
     msg.update({"sherpa_status": sherpa_status_update})
     msg.update({"station_status": station_status_update})
@@ -86,6 +91,6 @@ def send_periodic_updates():
             for fleet in all_fleets:
                 msg = get_fleet_status_msg(session, fleet)
                 send_status_update(msg)
-  
-            #send_status_update(trip_status)
+
+            # send_status_update(trip_status)
             time.sleep(1)
