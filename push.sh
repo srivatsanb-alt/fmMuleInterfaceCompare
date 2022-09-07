be95424c9ffc17d9325ee8936c1ae012da1f7816
Sai Karthik <pixarsnewhero@gmail.com>
2022-09-07 11:07:28 +0530

    docker-compose: Updating docker-compose for fm

--- push.sh
+++ push.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-export DOCKER_HOST=ssh://ati@$1
+# export DOCKER_HOST=ssh://ati@$1
+# export DOCKER_HOST=ssh://ati@$1
 
 echo "Pushing to $1"
 
 IMAGE_ID=$(docker images --format '{{.ID}} {{.Repository}} {{.Tag}}' fleet_manager)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-GIT_TAG="$(git rev-parse HEAD) $(git diff --quiet || echo 'dirty')" 
+GIT_TAG="$(git rev-parse HEAD) $(git diff --quiet || echo 'dirty')"
 IMAGE_ID="$IMAGE_ID built on $USER@$(hostname) from $GIT_TAG branch $BRANCH $(date)"
 
 echo "IMAGE_ID: $IMAGE_ID"
@@ -19,14 +20,9 @@ docker image build --build-arg image_meta="${IMAGE_ID}" -t fleet_manager:$GIT_TA
 
 # Stop the container if it's already running
 echo "Stopping and removing old fleet manager docker image"
-docker stop fleet_manager 
+docker stop fleet_manager
 docker rm fleet_manager
 
 echo "Running docker image on the server $1"
-# docker run -d \
-#        --name fm_container \
-#        # -v /tmp:/tmp \ # IS THIS NEEDED? IS THIS CORRECT?
-#        -v ~/fleet_manager/logs:/logs \
-#        -v ~/fleet_manager/static:/static \
-#        -v ~/fleet_manager/mule/config: /app/mule/config \
-#        fleet_manager
+
+docker compose up
