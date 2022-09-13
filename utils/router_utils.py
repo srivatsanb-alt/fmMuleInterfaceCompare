2f3ed999728e29339a0434f38f0c405f1e4f10ee
Sai Karthik <pixarsnewhero@gmail.com>
2022-09-13 17:55:18 +0530

    router_utils: Added new module to utils

--- utils/router_utils.py
+++ utils/router_utils.py
@@ -2,11 +2,16 @@ from mule.ati.control.bridge.router_planner_interface import RoutePlannerInterfa
 
 
 class RouterModule:
-    router: None
+    router = RoutePlannerInterface().router()
 
     @classmethod
-    def get_path_lines(cls, start_pose, dest_poses):
-        if not cls.router:
-            rpi = RoutePlannerInterface()
-            cls.router = rpi.router
+    def get_path_wps(cls, start_pose, dest_poses):
         return cls.router.generate_path_wps_for_viz(start_pose, dest_poses)
+
+    @classmethod
+    def get_route_length(cls, start_pose, end_pose):
+        return cls.router.get_route_length(start_pose, end_pose)
+
+    @classmethod
+    def get_route(cls, start_pose, end_pose):
+        return cls.router.solve_route(start_pose, end_pose)
