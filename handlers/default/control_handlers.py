19994113a746501df6732586c7b04950f0b09b07
srikarthikeyank <srikarthikeyank@gmail.com>
2022-10-11 14:45:45 +0530

    removed control_handlers

--- handlers/default/control_handlers.py
+++
@@ -1,9 +0,0 @@
-from utils.router_utils import RouterModule
-from models.request_models import GiveRouteWPS
-
-router: RouterModule = RouterModule()
-
-
-class ControlHandlers:
-    def give_route_wps(self, wps_req: GiveRouteWPS):
-        return router.get_path_wps(wps_req.start_pose, wps_req.to_poses)
