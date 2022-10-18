43652706b53cb538ec787f66d8cead7301c4df98
srikarthikeyank <srikarthikeyank@gmail.com>
2022-10-18 17:53:40 +0530

    copy docker-compose.yml along with static files

--- scripts/push_utils.sh
+++ scripts/push_utils.sh
@@ -38,6 +38,7 @@ create_static_backup()
 
   ssh $usr_name@$ip_address "rsync -aP /home/$usr_name/static/. /home/$usr_name/static_old/."
   rsync -azP ./static/* $usr_name@$ip_address:/home/$usr_name/static/.
+  rsync -azP docker-compose.yml $usr_name@$ip_address:/home/$usr_name/static/.
   echo "setting env variable FM_SERVER_IP $FM_SERVER_IP"
   ssh $usr_name@$ip_address "export FM_SERVER_IP=$ip_address"
   echo "Set env variable FM_SERVER_IP "
