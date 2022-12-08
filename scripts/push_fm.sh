917165efe8264098003b49bdd18de63d1d6d7d5a
Sai Karthik <pixarsnewhero@gmail.com>
2022-12-08 13:41:19 +0530

    push_fm: Modified image info

--- scripts/push_fm.sh
+++ scripts/push_fm.sh
@@ -96,8 +96,12 @@ if [ $clear_db == 1 ] ; then
 fi
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
+GIT_COMMIT="$(git rev-parse HEAD)"
 GIT_TAG="$(git rev-parse HEAD) $(git diff --quiet || echo 'dirty')"
-FM_IMAGE_INFO="Image built on $USER@$(hostname)_from $GIT_TAG branch $BRANCH_$(date)"
+IS_DIRTY="$(git diff --quiet || echo 'dirty')"
+FM_IMAGE_INFO="FM image built on $USER@$(hostname) branch $BRANCH $GIT_COMMIT (tags $GIT_TAG) IS_DIRTY $IS_DIRTY $(date)" 
+
+
 echo "FM_IMAGE_INFO: $FM_IMAGE_INFO"
 
 echo "Building fleet manager docker image"
