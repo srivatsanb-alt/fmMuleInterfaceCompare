dfebe825a41050f7c0633adcad298944119255e4
srikarthikeyank <srikarthikeyank@gmail.com>
2023-01-30 15:19:53 +0530

    updated load_docker_images.sh

--- scripts/load_docker_images.sh
+++ scripts/load_docker_images.sh
@@ -1,9 +1,18 @@
-echo "loading nginx image"
-docker image load -i nginx_1_14_0.tar
-echo "loaded nginx image"
 echo "loading postgres image"
 docker image load -i postgres_14_0.tar
 echo "loaded postgres image"
+echo "loading registry v2 image"
+docker image load -i registry_v2.tar
+echo "loaded registry_v2 image"
+echo "loading fm nginx image"
+docker image load -i fm_nginx.tar
+echo "loaded fm nginx image"
 echo "loading fm_base image"
 docker image load -i fm_base.tar
 echo "loaded fm_base image"
+echo "loading fm_final image"
+{
+   docker image load -i fm_final.tar
+   echo "loaded fm final image successfully"
+} || echo "Unable to fm final image"
+
