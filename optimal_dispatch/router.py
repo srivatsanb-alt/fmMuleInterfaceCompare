708c7898f21e2041ec7914b12f6863991f5bea1c
srikarthikeyank <srikarthikeyank@gmail.com>
2023-03-08 11:10:40 +0530

    moved get router_wps  inside try catch block

--- optimal_dispatch/router.py
+++ optimal_dispatch/router.py
@@ -51,29 +51,29 @@ def start_router_module():
             redis_conn.delete(key)
 
         for key in redis_conn.scan_iter("control_router_wps_job_*"):
-            str_job = redis_conn.get(key)
-            logger.info(f"got a route preview estimation job {str_job}")
-            control_router_wps_job = json.loads(str_job)
-            station_poses = control_router_wps_job[0]
-            fleet_name = control_router_wps_job[1]
-            job_id = control_router_wps_job[2]
-            rm = all_router_modules.get_router_module(fleet_name)
-
-            start_pose = station_poses[0]
-            dest_poses = station_poses[1:]
-
             try:
+                str_job = redis_conn.get(key)
+                logger.info(f"got a route preview estimation job {str_job}")
+                control_router_wps_job = json.loads(str_job)
+                station_poses = control_router_wps_job[0]
+                fleet_name = control_router_wps_job[1]
+                job_id = control_router_wps_job[2]
+                rm = all_router_modules.get_router_module(fleet_name)
+
+                start_pose = station_poses[0]
+                dest_poses = station_poses[1:]
                 wps_list = rm.get_path_wps(start_pose, dest_poses)
+                redis_conn.set(f"result_wps_job_{job_id}", json.dumps(wps_list))
+                logger.info(f"result of wps req: {wps_list}")
+                redis_conn.delete(key)
+
             except Exception as e:
-                logger.info(f"unable to get route for poses {station_poses} \n Exception {e}")
+                logger.info(
+                    f"unable to get route for poses {station_poses} \n Exception {e}"
+                )
                 wps_list = []
 
-            redis_conn.set(f"result_wps_job_{job_id}", json.dumps(wps_list))
-            logger.info(f"result of wps req: {wps_list}")
-            redis_conn.delete(key)
-        
         time.sleep(1e-2)
-
 
 
 if __name__ == "__main__":
