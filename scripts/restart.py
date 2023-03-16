7132142890d8b03ba3097392346b48297a1ed9e4
srikarthikeyank <srikarthikeyank@gmail.com>
2023-03-16 14:24:16 +0530

    added sleep to restart.py

--- scripts/restart.py
+++ scripts/restart.py
@@ -1,23 +1,25 @@
 import redis
 import json
 import os
+import time
 import logging
 
 
 def main():
-    while True:
-        redis_conn = redis.from_url(os.getenv("FM_REDIS_URI"))
-        restart = False
-        logging.getLogger().info("Will look for restart_fm key in redis")
-        while not restart:
-            restart = redis_conn.get("restart_fm")
-            if restart is None:
-                continue
-            restart = json.loads(restart)
+    redis_conn = redis.from_url(os.getenv("FM_REDIS_URI"))
+    redis_conn.set("restart_fm", json.dumps(False))
+    restart = False
+    logging.getLogger().info("Will look for restart_fm key in redis")
+    while not restart:
+        restart = redis_conn.get("restart_fm")
+        if restart is None:
+            continue
+        restart = json.loads(restart)
+        if restart:
+            logging.getLogger().info("Will restart fleet manager software")
+            raise Exception("Will restart fleet manager software")
 
-            if restart:
-                logging.getLogger().info("Will restart fleet manager software")
-                raise Exception("Will restart fleet manager software")
+        time.sleep(1e-2)
 
 
 if __name__ == "__main__":
