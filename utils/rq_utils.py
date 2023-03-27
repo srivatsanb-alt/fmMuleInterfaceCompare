946bba49e840a9b72b192353dcac8e0052870fda
srikarthikeyank <srikarthikeyank@gmail.com>
2023-03-27 16:34:15 +0530

    increased sleep in router.py, added kwargs to handle in dependencies

--- utils/rq_utils.py
+++ utils/rq_utils.py
@@ -72,10 +72,7 @@ def enqueue(queue: Queue, func, *args, **kwargs):
     kwargs.setdefault("timeout", 15)
     kwargs.setdefault("on_failure", report_failure)
     kwargs.setdefault("on_success", report_success)
-    return queue.enqueue(
-        func,
-        *args,
-    )
+    return queue.enqueue(func, *args, **kwargs)
 
 
 def enqueue_at(queue: Queue, dt, func, *args, **kwargs):
