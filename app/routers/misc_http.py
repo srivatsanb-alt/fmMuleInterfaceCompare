f6287a4dac37e3be1246c87cb5386019f2fed435
srikarthikeyank <srikarthikeyank@gmail.com>
2023-05-15 12:18:55 +0530

    check if sherpa pose is None in sherpa summary

--- app/routers/misc_http.py
+++ app/routers/misc_http.py
@@ -131,11 +131,12 @@ async def sherpa_summary(
         at_station = None
         response.update({"at_station": at_station})
 
-        all_stations = dbsession.get_all_stations()
-        for station in all_stations:
-            if utils_util.are_poses_close(sherpa_status.pose, station.pose):
-                at_station = station
-                break
+        if sherpa_status.pose is not None:
+            all_stations = dbsession.get_all_stations()
+            for station in all_stations:
+                if utils_util.are_poses_close(sherpa_status.pose, station.pose):
+                    at_station = station
+                    break
 
         if at_station:
             response.update({"at_station": at_station.name})
