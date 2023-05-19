2f66f61bfd4fbae09b41eda4ac3a5a8926f3f8e3
srikarthikeyank <srikarthikeyank@gmail.com>
2023-05-19 16:33:28 +0530

    added process time to process_time middleware

--- app/main.py
+++ app/main.py
@@ -55,6 +55,9 @@ async def add_process_time_header(request: Request, call_next):
     response = await call_next(request)
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
+    logging.getLogger("process_times").info(
+        f"Process time - url: {request.url}: {process_time*1000} ms"
+    )
     return response
 
 
