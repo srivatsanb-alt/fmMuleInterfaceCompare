98515cbadd2ba50fc53d8be178be8fa08a474cd9
srikarthikeyank <srikarthikeyank@gmail.com>
2023-05-29 11:18:01 +0530

    log message rejected msg in status_updates logger

--- app/routers/sherpa_ws.py
+++ app/routers/sherpa_ws.py
@@ -140,7 +140,7 @@ async def reader(websocket, sherpa):
 
         ok, reason = accept_message(sherpa, msg)
         if not ok:
-            logging.warn(
+            logging.getLogger("status_updates").warning(
                 f"message rejected type={msg_type},ts={ts},sherpa={sherpa},reason={reason}"
             )
             continue
