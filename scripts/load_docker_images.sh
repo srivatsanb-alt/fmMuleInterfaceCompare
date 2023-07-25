e8b918f63e1d09a317e76509e579cb1477796831
srikarthikeyank <srikarthikeyank@gmail.com>
2023-07-25 14:36:16 +0530

    accpet docker network arg in load_docker_images script

--- scripts/load_docker_images.sh
+++ scripts/load_docker_images.sh
@@ -20,20 +20,20 @@ fi
 
 # base images
 echo "loading postgres image"
-#docker image load -i postgres_14_0.tar
+docker image load -i postgres_14_0.tar
 echo "loaded postgres image"
 echo "loading registry v2 image"
-#docker image load -i registry_v2.tar
+docker image load -i registry_v2.tar
 echo "loaded registry_v2 image"
 echo "loading nginx:1.23.3 image"
-#docker image load -i nginx_1_23_3.tar
+docker image load -i nginx_1_23_3.tar
 echo "loaded nginx:1.23.3 image"
 echo "loading fm_base image"
-#docker image load -i fm_base.tar
+docker image load -i fm_base.tar
 echo "loaded fm_base image"
 
 echo "loading grafana_9_5_2 image"
-#docker image load -i grafana_9_5_2.tar
+docker image load -i grafana_9_5_2.tar
 echo "loaded grafana_9_5_2 image"
 
 echo "loading fm_final image"
