e67e72f03c5d9a107dadd020d33ca7ed0a07dfa5
srikarthikeyank <srikarthikeyank@gmail.com>
2023-08-03 13:46:11 +0530

    trying out setup config

--- core/config_validator.py
+++ core/config_validator.py
@@ -11,24 +11,85 @@ class ConfigValidator:
                 "permission_level",
             ],
             "properties": {
-                "method": {"type": "string", "enum": ["hungarian"]},
+                "method": {
+                    "type": "string",
+                    "enum": ["hungarian"],
+                    "description": "Algorithm used to solve taxi dispatch problem",
+                },
                 "prioritise_waiting_stations": {
                     "type": "boolean",
+                    "description": "If set to True, multiply trip priority with a factor calculated based on wait time(time since booking)",
                 },
-                "eta_power_factor": {"type": "number", "minimum": 0.1, "maximum": 1.0},
+                "eta_power_factor": {
+                    "type": "number",
+                    "minimum": 0.1,
+                    "maximum": 1.0,
+                    "description": "Modifier - actual eta will be modified to eta^eta_power_factor",
+                },
                 "priority_power_factor": {
                     "type": "number",
                     "minimum": 0.1,
                     "maximum": 1.0,
+                    "description": "Modifier - actual trip priority will be modified to trip_priority^priority_power_factor",
                 },
                 "max_trips_to_consider": {
                     "type": "number",
                     "minimum": 1,
                     "maximum": 20,
+                    "description": "Max number of trips that will be considered for optimal dispatch, decrease to lessen conputational cost",
                 },
-                "permission_level": {"type": "number", "minimum": 1, "maximum": 3},
+                "permission_level": {
+                    "type": "number",
+                    "minimum": 1,
+                    "maximum": 3,
+                    "description": "For role based access",
+                },
             },
         },
+    }
+
+    backup = {
+        "$jsonSchema": {
+            "type": "object",
+            "required": [
+                "keep_size_mb",
+            ],
+            "properties": {
+                "keep_size_mb": {
+                    "type": "number",
+                    "description": "will make sure disk space used by static/data_backup folder size(MB) is less than keep_size_mb",
+                }
+            },
+        }
+    }
+    comms = {
+        "$jsonSchema": {
+            "type": "object",
+            "required": [
+                "mule_heartbeat_interval",
+            ],
+            "properties": {
+                "mule_heartbeat_interval": {
+                    "type": "number",
+                    "description": "mule will be considered disconnected, if no sherpa_status message was received in the last mule_heartbeat_interval seconds",
+                }
+            },
+        }
+    }
+
+    simulator = {
+        "$jsonSchema": {
+            "type": "object",
+            "required": [
+                "simulate",
+                "book_trips",
+                "visa_handling",
+                "speedup_factor",
+                "average_velocity",
+                "pause_at_station",
+                "conveyor_capacity",
+            ],
+        }
     }
 
 
@@ -40,4 +101,15 @@ class ConfigDefaults:
         "priority_power_factor": 0.7,
         "max_trips_to_consider": 5,
         "permission_level": 1,
+    }
+    backup = {"keep_size_mb": 1000}
+    comms = {"mule_heartbeat_interval": 60}
+    simulator = {
+        "simulate": False,
+        "book_trips": False,
+        "visa_handling": False,
+        "speedup_factor": 1.0,
+        "average_velocity": 0.8,
+        "pause_at_station": 10.0,
+        "conveyor_capacity": 6,
     }
--- static/setup_config.py
+++ static/setup_config.py
@@ -1,22 +1,19 @@
-from core.mongo_db import FMMongo 
-from core.config_validator import ConfigValidator
+from core.mongo_db import FMMongo
+from core.config_validator import ConfigValidator, ConfigDefaults
 
 fm_mongo = FMMongo()
 fc_db = fm_mongo.get_database("fm_config")
-fm_mongo.create_collection("optimal_dispatch", fc_db)
-fm_mongo.add_validator("optimal_dispatch", fc_db, ConfigValidator.optimal_dispatch)
 
+all_collection_names = ["optimal_dispatch", "comms", "backup", "simulator"]
 
-c = fm_mongo.get_collection("optimal_dispatch", fc_db)
-
-
-new_config = {
-    "method": "hungarian1",
-    "prioritise_waiting_stations": True, 
-    "eta_power_factor": 0.1,
-    "priority_power_factor": 1.7,
-    "max_trips_to_consider": 10,
-    "permission_level": 3,
-}
-
-c.insert_one(new_config)
+for collection_name in all_collection_names:
+    fm_mongo.create_collection(collection_name, fc_db)
+    fm_mongo.add_validator(collection_name, fc_db, getattr(ConfigValidator, collection_name))
+    c = fm_mongo.get_collection(collection_name, fc_db)
+    try:
+        c.find_one_and_replace({}, getattr(ConfigDefaults, collection_name))
+        print(f"replaced {collection_name}")
+    except Exception as e:
+        print("Unable to replace")
+        c.insert_one(getattr(ConfigDefaults, collection_name))
+        print(f"Inserted {collection_name}")
