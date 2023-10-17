ead1464fe7dee9d50c8e60e8ffe7664d42a32254
srikarthikeyank <srikarthikeyank@gmail.com>
2023-10-17 12:27:32 +0530

    added set timezone to pull script

--- scripts/pull_from_master_fm.sh
+++ scripts/pull_from_master_fm.sh
@@ -1,5 +1,13 @@
 set -e
 
+set_timezone()
+{
+    CONTINENT=$(echo $TZ | awk -F '/' '{ print $1 }')
+    CITY=$(echo $TZ | awk -F '/' '{ print $2 }')
+    sed -i "s/Asia/$CONTINENT/g" static/docker_compose_v$FM_VERSION.yml
+    sed -i "s/Asia/$CITY/g" static/docker_compose_v$FM_VERSION.yml
+}
+
 download_pull_fm_update()
 {
   redis-cli -p $REDIS_PORT set update_done false
@@ -12,7 +20,6 @@ download_pull_fm_update()
    MASTER_FM_REGISTRY_PASSWORD=$6
    STATIC_FILE_AUTH_USERNAME=$7
    STATIC_FILE_AUTH_PASSWORD=$8
-
    echo -e "will try to download, pull fm: $FM_VERSION from $HTTP_SCHEME://$MASTER_FM_IP:$MASTER_FM_PORT"
    docker login --username $MASTER_FM_REGISTRY_USERNAME --password $MASTER_FM_REGISTRY_PASSWORD  $MASTER_FM_IP:$MASTER_FM_PORT
    download_dc_yml=$(curl -o /app/static/docker_compose_v$FM_VERSION.yml -w "%{http_code}" $HTTP_SCHEME://$STATIC_FILE_AUTH_USERNAME:$STATIC_FILE_AUTH_PASSWORD@$MASTER_FM_IP:$MASTER_FM_PORT/api/static/downloads/fm/$FM_VERSION/docker_compose_v$FM_VERSION.yml)
@@ -22,7 +29,8 @@ download_pull_fm_update()
       exit 1
    }
    fi
-
+   set_timezone
+   #cp /app/static/docker_compose_v$FM_VERSION.yml /app/static/docker_compose_latest.yml
    docker-compose -f /app/static/docker_compose_v$FM_VERSION.yml config | grep image | awk -v registry="$MASTER_FM_IP:$MASTER_FM_PORT/" '{print registry$2}' | xargs -I % echo "will download %"
    docker-compose -f /app/static/docker_compose_v$FM_VERSION.yml config | grep image | awk -v registry="$MASTER_FM_IP:$MASTER_FM_PORT/" '{print registry$2}' | xargs -I % docker pull %
    docker-compose -f /app/static/docker_compose_v$FM_VERSION.yml config | grep image | awk '{print $2}' | xargs -I % docker tag "$MASTER_FM_IP:$MASTER_FM_PORT/"% %
