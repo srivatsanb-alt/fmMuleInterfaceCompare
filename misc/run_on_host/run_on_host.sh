854aad274fae18bc1784c690ed32f05faa0c5e98
srikarthikeyank <srikarthikeyank@gmail.com>
2023-10-18 14:45:02 +0530

    added run_on_host service to do docker-compose restart from UI

---
+++ misc/run_on_host/run_on_host.sh
@@ -0,0 +1,13 @@
+while true
+do
+    echo "run on host service is active!"
+    read service_type < run_on_host_fifo
+        echo "new service request: $service_type"
+        if [ $service_type = "restart_all_services" ]; then
+	    FM_VERSION=$(cat restart.with)
+	    docker-compose -p fm down || echo "Unable to stop FM"
+            docker-compose -p fm -f docker_compose_v$FM_VERSION.yml up -d || echo "Unable to bring up FM with $FM_VERSION"
+        else
+            echo "unknown service_type <$service_type>"
+        fi
+done
