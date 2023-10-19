29fdd77d050c1932487d57a845d650258d2d9e4e
srikarthikeyank <srikarthikeyank@gmail.com>
2023-10-19 13:31:56 +0530

    removed an echo log

--- misc/run_on_host/run_on_host.sh
+++ misc/run_on_host/run_on_host.sh
@@ -5,10 +5,9 @@ do
         echo "new service request: $service_type"
         if [ $service_type = "restart_all_services" ]; then
 	    FM_VERSION=$(cat restart.with)
-	    docker-compose -p fm down || echo "Unable to stop FM"
-	    echo "Stopped FM"
-	    sleep 3
+	    docker-compose -p fm -f docker_compose_v$FM_VERSION.yml down || echo "Unable to stop FM"
             docker-compose -p fm -f docker_compose_v$FM_VERSION.yml up -d || echo "Unable to bring up FM with $FM_VERSION"
+            echo "Restarted FM"
         else
             echo "unknown service_type <$service_type>"
         fi
