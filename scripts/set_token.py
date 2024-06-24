620b1713ee89268f14bcde15463f0fd80e17aff1
MohitAnand9 <mohit.anand@atimotors.com>
2024-06-24 12:21:52 +0530

    added try and except block for getting secret_token

--- scripts/set_token.py
+++ scripts/set_token.py
@@ -6,16 +6,16 @@ from models.mongo_client import FMMongo
 
 def main():
     token = secrets.token_urlsafe(32)
-    with FMMongo() as fm_mongo:
-        app_security_params = fm_mongo.get_document_from_fm_config("app_security")
-    
-    if app_security_params.get("secret_token"):
-        token = app_security_params.get("secret_token")
-        
+    try:
+        with FMMongo() as fm_mongo:
+            app_security_params = fm_mongo.get_document_from_fm_config("app_security")        
+        if app_security_params.get("secret_token"):
+            token = app_security_params.get("secret_token")
+    except:
+        pass 
     with redis.from_url(os.getenv("FM_REDIS_URI")) as redis_conn:
         redis_conn.set("FM_SECRET_TOKEN", token)
     print(f"Set FM_SECRET_TOKEN")
-
 
 if __name__ == "__main__":
     main()
